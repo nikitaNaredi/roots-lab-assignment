a16b349b65acca31e445678fe1669c7aa0b097b1
Nikita  Kumari <nikita.kumari@CALSOFTHQ.COM>
2019-08-23 16:03:15 +0530

    sorting

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -17,6 +17,33 @@ export class TableComponent implements OnInit {
   tableLength: number;
   currentPage: number;
   tablePages: number;
+  icon = [ "arrow_upward", "arrow_downward"];
+  headerJson = [
+      {
+        'label':'First Name',
+        'name': 'first',
+        'order' : 0},
+      {
+        'label':'Last Name',
+        'name': 'last',
+        'order' : 0},
+      {
+        'label':'Email ID',
+        'name': 'email',
+        'order' : 0}, 
+      {
+        'label':'Address',
+        'name': 'address',
+        'order' : 0},
+        {
+          'label':'Created',
+          'name': 'created',
+          'order' : 0},
+      {
+        'label':'Balance',
+        'name': 'balance',
+        'order' : 0}
+      ] 
 
   constructor(private service: DataService, private http: HttpClient){
     
@@ -40,15 +67,25 @@ export class TableComponent implements OnInit {
     console.log(this.currentPage);
   }
 
-  sort(sortBy: string) {
-    console.log(sortBy);
+  updateOrderBy(name) {
+    if(this.headerJson[name].order == 0)
+      this.headerJson[name].order = 1;
+    else
+      this.headerJson[name].order = 0;
+  }
+
+  sort(sortBy: string, orderBy: number) {
     this.tableFilter = (this.tableData).sort( (a, b ) => {
-      console.log("oinssdf "+a+" "+b.first);
-      if (a[name] > b[name]) 
-        return -1;
+      var val;
+      if (a[sortBy] > b[sortBy]) 
+        val = (orderBy == 0) ? -1 : 1;
       else if(a[sortBy] < b[sortBy]) 
-        return 1;
-      return 0;
+        val = (orderBy == 0) ? 1 : -1;
+      else 
+        val = 0;
+      return val;
     });
   }
+
+  
 }
